86394ec02f35e5dd928a4041029949f929bcad28
Naufal Ulhaq <naufalulhaq.13@gmail.com>
2023-06-13 12:33:59 +0700

    change output of predict, modify request.py

--- using_flask/model.py
+++ using_flask/model.py
@@ -4,10 +4,18 @@ from PIL import Image
 import json
 
 # Load the models
-model_allergic  = load_model('models/allergic vs non allergic/my_model.h5')
+model_allergic = load_model('models/allergic vs non allergic/my_model.h5')
 model_bacterial = load_model('models/bacterial vs non bacterial/model_bacterial.h5')
-model_fungal    = load_model('models/fungal vs non fungal/my_model.h5')
-model_healthy   = load_model('models\healthy vs non healthy\model_healthy.h5')
+model_fungal = load_model('models/fungal vs non fungal/my_model.h5')
+model_healthy = load_model('models/healthy vs non healthy/model_healthy.h5')
+
+# Function to preprocess the image
+def load_and_preprocess_image(image_path):
+    image = Image.open(image_path)
+    image = image.resize((128, 128))
+    image = np.expand_dims(image, axis=0)
+    image = image / 255.0  # Normalize the image
+    return image
 
 # Function to predict an image using all four models
 def predict_image(image_path):
@@ -20,29 +28,21 @@ def predict_image(image_path):
     prediction3 = model_fungal.predict(image)[0][0]
     prediction4 = model_healthy.predict(image)[0][0]
 
+    # Round the predictions and convert to binary status
+    status1 = int(round(prediction1))
+    status2 = int(round(prediction2))
+    status3 = int(round(prediction3))
+    status4 = int(round(prediction4))
+
     # Format the predictions as a dictionary
-    results = {'model_allergic': float(prediction1),
-               'model_bacterial': float(prediction2),
-               'model_fungal': float(prediction3),
-               'model_healthy': float(prediction4)}
+    results = {
+        'model_allergic': {'prediction': float(prediction1), 'status': status1},
+        'model_bacterial': {'prediction': float(prediction2), 'status': status2},
+        'model_fungal': {'prediction': float(prediction3), 'status': status3},
+        'model_healthy': {'prediction': float(prediction4), 'status': status4}
+    }
 
     # Convert the dictionary to JSON format
     json_results = json.dumps(results)
 
     return json_results
-
-# Function to load and preprocess the image
-def load_and_preprocess_image(image_path):
-    # Load the image using your preferred method (e.g., PIL, OpenCV)
-    # Preprocess the image as needed (e.g., resize, normalize)
-
-    # Load the image using PIL
-    image = Image.open(image_path)
-
-    # Preprocess the image (resize, normalize, etc.)
-    image = image.resize((128, 128))  # Example: Resize the image to (224, 224)
-    image = np.array(image) / 255.0  # Example: Normalize pixel values between 0 and 1
-
-    # Return the preprocessed image as a numpy array
-    return np.expand_dims(image, axis=0)
-
--- using_flask/request.py
+++ using_flask/request.py
@@ -1,21 +1,28 @@
 import requests
+import os
 
 # URL of your Flask API
 url = 'http://127.0.0.1:5000/predict'  # Update with your API URL
 
-# Path to the image file
-image_path = 'images\example_allergic.jpg'  # Update with the actual image path
+# Directory containing the images
+image_dir = 'images'  # Update with the actual image directory
 
-# Create a dictionary with the image file
-files = {'image': open(image_path, 'rb')}
+# Iterate through the images in the directory
+for image_file in os.listdir(image_dir):
+    image_path = os.path.join(image_dir, image_file)
 
-# Make the POST request
-response = requests.post(url, files=files)
+    # Create a dictionary with the image file
+    files = {'image': open(image_path, 'rb')}
 
-# Check the response
-if response.status_code == 200:
-    # Print the prediction results
-    print(response.json())
-else:
-    # Print the error message
-    print('Error:', response.text)
+    # Make the POST request
+    response = requests.post(url, files=files)
+
+    # Check the response
+    if response.status_code == 200:
+        # Print the image's name and the API output
+        print('Image:', image_file)
+        print('API Output:', response.json())
+        print()
+    else:
+        # Print the error message
+        print('Error:', response.text)
